0b7d5ec95a2c36160defe1263f809107c41d1ac1
Richard Harris <richard@idestini.com>
2019-12-01 18:51:23 -0600

    Build

---
+++ build/precache-manifest.622f8bc8d8adf0b20d97d636554b6658.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a954b5c2aaec02f675e16254b52c594",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbf36eb8dd2f1c96a34a",
+    "url": "/static/css/main.cddc986a.chunk.css"
+  },
+  {
+    "revision": "030edf389ea479bece9f",
+    "url": "/static/js/2.6731af6c.chunk.js"
+  },
+  {
+    "revision": "dbf36eb8dd2f1c96a34a",
+    "url": "/static/js/main.0beb2d3b.chunk.js"
+  },
+  {
+    "revision": "5d4ece6c64b48825b3c0",
+    "url": "/static/js/runtime-main.54d04848.js"
+  }
+]);
